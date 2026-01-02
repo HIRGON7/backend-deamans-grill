d5ca5fe91607a7ee0d63b16edf077af35132b483
hadi jawhari <126268930+HIRGON7@users.noreply.github.com>
2026-01-02 16:21:54 +0200

    Update db.js

--- db.js
+++ db.js
@@ -5,14 +5,16 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("MA2TOU3A", err.message);
+    console.error("DB ERROR:", err.message);
   } else {
-    console.log("DAWLE");
+    console.log("DB CONNECTED");
   }
 });
 
