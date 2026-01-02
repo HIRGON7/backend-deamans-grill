4f8febd4f372115ce5b6e17db6cc5397288267ee
hadi jawhari <126268930+HIRGON7@users.noreply.github.com>
2026-01-02 17:14:23 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,23 +1,13 @@
-const mysql = require("mysql2");
+import mysql from "mysql2";
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error("❌ MySQL connection error:", err);
-    process.exit(1); // IMPORTANT
-  } else {
-    console.log("✅ Connected to Railway MySQL");
+    console.error("MySQL connection error:", err);
+    process.exit(1);
   }
+  console.log("MySQL connected");
 });
 
-module.exports = db;
+export default db;
